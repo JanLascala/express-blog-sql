7f64874d0aa0514cce6be5210d61ba6f24fc19ca
JanLascala <lascala.jan@gmail.com>
2025-04-14 15:22:15 +0200

    Update database connection to use environment variables and modify watch script to load .env

--- data/db.js
+++ data/db.js
@@ -1,13 +1,10 @@
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog'
-});
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL!');
-});
-module.exports = connection;
+const connection = mysql.createConnection({
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+})
