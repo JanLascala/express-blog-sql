1d692c16be62ad144f5dd601a9fcfe8e63277c38
JanLascala <lascala.jan@gmail.com>
2025-04-14 15:00:12 +0200

    Add MySQL connection setup and update dependencies in package files

--- data/db.js
+++ data/db.js
@@ -5,3 +5,9 @@ const connection = mysql.createConnection({
     password: '',
     database: 'blog'
 });
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL!');
+});
+module.exports = connection;
